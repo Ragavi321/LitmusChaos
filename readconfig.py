843c530850e083bd6d39f9726f6f88edb3cc5654
Ragavi321 <70432566+Ragavi321@users.noreply.github.com>
2022-06-30 17:46:21 +0530

    Update readconfig.py

--- readconfig.py
+++ readconfig.py
@@ -10,6 +10,6 @@ def load_config_file():
     config_file_name = read_config_file()
     file = open(config_file_name)
     config_data = json.load(file)
-    print(json.dumps(config_data))
+    print(json.dumps(config_data, indent=4))
     return config_data
     
